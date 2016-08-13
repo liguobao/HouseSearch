22bb433b108a9bc92282fc8bd98035a88e14a357
liguobao <codelover@qq.com>
2016-08-13 23:11:38 +0800

    delete useless code

--- 58HouseSearch/DomainJS/home.js
+++ 58HouseSearch/DomainJS/home.js
@@ -130,47 +130,6 @@ function Get58DataClick() {
 }
 
 
-
-function Location()
-{
-    var map, geolocation;
-    //加载地图，调用浏览器定位服务
-    map = new AMap.Map('container', {
-        resizeEnable: true
-    });
-    map.plugin('AMap.Geolocation', function () {
-        geolocation = new AMap.Geolocation({
-            enableHighAccuracy: true,//是否使用高精度定位，默认:true
-            timeout: 10000,          //超过10秒后停止定位，默认：无穷大
-            buttonOffset: new AMap.Pixel(10, 20),//定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-            zoomToAccuracy: true,      //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-            buttonPosition: 'RB',
-        });
-        map.addControl(geolocation);
-        geolocation.getCurrentPosition();
-        AMap.event.addListener(geolocation, 'complete', onComplete);//返回定位信息
-        AMap.event.addListener(geolocation, 'error', onError);      //返回定位出错信息
-    });
-    //解析定位结果
-}
-
-
-
-
-function onComplete(data) {
-
-    var str = ['所在城市：'];
-    str.push('经度：' + data.position.getLng());
-    str.push('纬度：' + data.position.getLat());
-    document.getElementById('IPLocation').innerHTML = str.join('<br>');
-
-}
-//解析定位错误信息
-function onError(data) {
-    document.getElementById('IPLocation').innerHTML = '定位失败';
-}
-
-
 function showCityInfo(map) {
     //实例化城市查询类
     var citysearch = new AMap.CitySearch();
