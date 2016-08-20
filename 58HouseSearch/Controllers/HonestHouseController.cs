309737bfb51bf8d5ddfc0512de64e7ab38e17908
liguobao <codelover@qq.com>
2016-08-21 00:33:16 +0800

    1. 修复一个获取数据正常，地图无法显示数据的bug（衣衣重构代码弄出来的...） 2. 经测试后发现，某些地图没有诚信房源数据（考虑这种情况是否直接取个人出租信息）

--- 58HouseSearch/Controllers/HonestHouseController.cs
+++ 58HouseSearch/Controllers/HonestHouseController.cs
@@ -30,13 +30,20 @@ namespace _58HouseSearch.Controllers
                 return Json(new { IsSuccess = false, Error = "城市定位失败，建议清除浏览器缓存后重新进入。" });
             }
            
+            try
+            {
+                var pageCount = GetPageNum(costFrom, costTo, cnName);
+                if (pageCount == 0)
+                    return Json(new { IsSuccess = false, Error = $"没有找到价格区间为{costFrom} - {costTo}的房源信息,大部分情况是因为该市没有诚信房源数据。" });
 
-            var pageCount = GetPageNum(costFrom, costTo, cnName);
-            if(pageCount == 0)
-                return Json(new { IsSuccess = false, Error = $"没有找到价格区间为{costFrom} - {costTo}的房子。" });
-
-            var roomList = Enumerable.Range(1, pageCount).Select(index => GetRoomList(costFrom, costTo, cnName, index)).Aggregate((a, b) => a.Concat(b));
-            return Json(new { IsSuccess = true, HouseInfos = roomList });
+                var roomList = Enumerable.Range(1, pageCount).Select(index => GetRoomList(costFrom, costTo, cnName, index)).Aggregate((a, b) => a.Concat(b));
+                return Json(new { IsSuccess = true, HouseInfos = roomList });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { IsSuccess = false, Error = "由于某种诡异的原因，爬取数据奔溃了...建议重试或者重新输入条件。下面是你看不懂的异常信息："+ex.ToString()  });
+            }
+          
 
          
 
@@ -59,7 +66,10 @@ namespace _58HouseSearch.Controllers
             var houseList = page.QuerySelectorAll("tr[logr]").Select(room =>
               new HouseInfo
               {
-                   HouseLocation=room.QuerySelector("a.a_xq1").TextContent.Replace("租房",""),
+                  // HouseLocation=room.QuerySelector("a.a_xq1").TextContent.Replace("租房",""),
+                  HouseLocation = room.QuerySelector("span.f12")!=null && !string.IsNullOrEmpty(room.QuerySelector("span.f12").TextContent)? 
+                  room.QuerySelector("span.f12").TextContent.Replace("租房", ""): room.QuerySelector("a.a_xq1").TextContent.Replace("租房", ""),
+                 
                    HouseTitle=room.QuerySelector("a.t").TextContent,
                    Money=room.QuerySelector("b.pri").TextContent,
                    HouseURL= $"http://{cnName}.58.com/zufang/{room.GetAttribute("logr").Split('_')[3]}x.shtml"
@@ -76,7 +86,7 @@ namespace _58HouseSearch.Controllers
                 int number = 0;
                 return int.TryParse(page.TextContent, out number) ? number : 0;
             });
-            return pageNums?.Max() ?? 0;
+            return pageNums!=null && pageNums.Count()!=0 ? pageNums.Max() : 0;
         }
 
     }
