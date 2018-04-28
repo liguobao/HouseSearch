7406d322f1d002452b0077131d9f9bd5451870bf
liguobao <liguobao@nanchao.org>
2018-04-28 11:28:27 +0800

    cache add try cache

--- HouseCrawler.Core/HouseCrawler.Web/Service/RedisService.cs
+++ HouseCrawler.Core/HouseCrawler.Web/Service/RedisService.cs
@@ -1,5 +1,6 @@
 ﻿using HouseCrawler.Web.DataContent;
 using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 
 namespace HouseCrawler.Web
@@ -8,55 +9,83 @@ namespace HouseCrawler.Web
     {
         public static List<DBHouseInfo> ReadSearchCache(string key)
         {
-            ConfigurationOptions options = ConfigurationOptions.Parse(ConnectionStrings.RedisConnectionString);
-            options.SyncTimeout =10 * 1000;
-            ConnectionMultiplexer redis = ConnectionMultiplexer .Connect(options);
-            IDatabase db = redis.GetDatabase();
-            if (db.KeyExists(key))
+            try
             {
-                string houseJson = db.StringGet(key);
-                return Newtonsoft.Json.JsonConvert.DeserializeObject<List<DBHouseInfo>>(houseJson);
+                ConfigurationOptions options = ConfigurationOptions.Parse(ConnectionStrings.RedisConnectionString);
+                options.SyncTimeout = 10 * 1000;
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(options);
+                IDatabase db = redis.GetDatabase();
+                if (db.KeyExists(key))
+                {
+                    string houseJson = db.StringGet(key);
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<List<DBHouseInfo>>(houseJson);
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch (System.Exception ex)
             {
+                LogHelper.Error("ReadSearchCache", ex);
                 return null;
             }
+
         }
 
         public static void WriteSearchCache(string key, List<DBHouseInfo> house)
         {
-            ConnectionMultiplexer redis = ConnectionMultiplexer
+            try
+            {
+                ConnectionMultiplexer redis = ConnectionMultiplexer
                 .Connect(ConnectionStrings.RedisConnectionString);
-            IDatabase db = redis.GetDatabase();
-            db.StringSet(key, Newtonsoft.Json.JsonConvert.SerializeObject(house), new System.TimeSpan(1,0,0));
+                IDatabase db = redis.GetDatabase();
+                db.StringSet(key, Newtonsoft.Json.JsonConvert.SerializeObject(house), new System.TimeSpan(0, 30, 0));
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error("WriteSearchCache", ex);
+            }
+
         }
 
 
 
         public static string ReadCache(string key)
         {
-            ConfigurationOptions options = ConfigurationOptions.Parse(ConnectionStrings.RedisConnectionString);
-            options.SyncTimeout = 10 * 1000;
-            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(options);
-            IDatabase db = redis.GetDatabase();
-            if (db.KeyExists(key))
+            try
             {
-                return db.StringGet(key);
-
+                ConfigurationOptions options = ConfigurationOptions.Parse(ConnectionStrings.RedisConnectionString);
+                options.SyncTimeout = 10 * 1000;
+                ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(options);
+                IDatabase db = redis.GetDatabase();
+                return db.KeyExists(key) == true ? db.StringGet(key).ToString() : null;
             }
-            else
+            catch (Exception ex)
             {
+                LogHelper.Error("ReadCache", ex);
                 return null;
             }
+
+
         }
 
 
         public static void WriteCache(string key, string value)
         {
-            ConnectionMultiplexer redis = ConnectionMultiplexer
+
+            try
+            {
+                ConnectionMultiplexer redis = ConnectionMultiplexer
                 .Connect(ConnectionStrings.RedisConnectionString);
-            IDatabase db = redis.GetDatabase();
-            db.StringSet(key, value, new System.TimeSpan(1, 0, 0));
+                IDatabase db = redis.GetDatabase();
+                db.StringSet(key, value, new System.TimeSpan(0, 30, 0));
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error("WriteCache", ex);
+            }
+
         }
     }
 }
