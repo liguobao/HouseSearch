a116f8d4416e1c7bfc1d68e64d4246d9db84df55
liguobao <liguobao@nanchao.org>
2018-05-13 22:11:07 +0800

    remove youmeng pic

--- HouseCrawler.Core/HouseCrawler.Web/wwwroot/DomainJS/umeng.js
+++ HouseCrawler.Core/HouseCrawler.Web/wwwroot/DomainJS/umeng.js
@@ -1,6 +1,6 @@
 ﻿
     var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
     document.write(unescape("%3Cspan style='display:none' id='cnzz_stat_icon_1260881876'%3E%3C/span%3E%3Cscript src='" +
-        cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1260881876%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
+        cnzz_protocol + "s95.cnzz.com/z_stat.php%3Fid%3D1260881876' type='text/javascript'%3E%3C/script%3E"));
 
 
