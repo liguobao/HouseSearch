de0c7f56058c394d8ab2973450f3ea97e77ae5ab
liguobao <liguobao@nana-tech.com>
2018-09-09 00:15:13 +0800

    add address api

--- HouseCrawler.Core/HouseCrawler.Web/Controllers/API/UserController.cs
+++ HouseCrawler.Core/HouseCrawler.Web/Controllers/API/UserController.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Options;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Cors;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace HouseCrawler.Web.API.Controllers
 {
@@ -55,6 +56,23 @@ namespace HouseCrawler.Web.API.Controllers
                 return Ok(new { success = true, data = userInfo });
             }
             return Ok(new { success = false, error = "用户不存在/鉴权失败!" });
+        }
+
+        [EnableCors("APICors")]
+        [HttpPost("api/users/{userId}/address", Name = "SetWorkAddress")]
+        public ActionResult SetWorkAddress(long userId, [FromHeader] string token, [FromBody]JToken model)
+        {
+            if (string.IsNullOrEmpty(model?["address"]?.ToString()))
+            {
+                return Ok(new { success = false, error = "地址不能为空!" });
+            }
+            var userInfo = userService.GetUserInfo(userId, token);
+            if (userInfo == null)
+            {
+                return Ok(new { success = false, error = "用户不存在/鉴权失败!" });
+            }
+            var success = userDataDapper.SaveWorkAddress(userId, model?["address"]?.ToString());
+            return Ok(new { success = success });
         }
     }
 }
