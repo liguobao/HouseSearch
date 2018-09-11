e0932a58687357c677dc688da7cc10d6c7783fd7
Erane <466540652@qq.com>
2018-09-11 22:15:50 +0800

    feat:loading动画

--- House-Map.UI/src/views/third-party-login.vue
+++ House-Map.UI/src/views/third-party-login.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    第三方登录中...
+  <div class="social" v-loading.fullscreen.lock="fullscreenLoading">
+
   </div>
 </template>
+<style scoped lang="scss">
+
+</style>
 <script>
   export default {
     methods: {
@@ -10,17 +13,25 @@
         const params = Object.assign({}, options);
         const data = await this.$ajax.get(`/account/callback?${this.$qs.stringify(params)}`);
         this.$store.dispatch('UserLogin', data);
+        this.$message.success('登录成功');
+        this.fullscreenLoading = false;
         this.$router.replace('/')
       }
     },
     created() {
       const query = this.$route.query;
-      if(query.code && query.state) {
+      if (query.code && query.state) {
+        this.fullscreenLoading = true;
         this.login({
           code: query.code,
           state: query.state
         })
+      }else {
+        this.$router.replace('/')
       }
+    },
+    mounted() {
+
     }
   }
 </script>
