04bef10442a530bc05cb78ae85c8590f3c4f9370
liguobao <liguobao@nana-tech.com>
2018-09-11 08:43:01 +0800

    add AnnouncementDapper

---
+++ HouseCrawler.Core/HouseCrawler.Web/DBEntity/Announcement.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HouseCrawler.Web
+{
+    public class Announcement
+    {
+        public long Id { get; set; }
+
+
+        public string Content { get; set; }
+        public DateTime DataCreateTime { get; set; }
+
+        public DateTime EndShowDate { get; set; }
+
+
+        public DateTime DataChange_LastTime { get; set; }
+
+    }
+}
