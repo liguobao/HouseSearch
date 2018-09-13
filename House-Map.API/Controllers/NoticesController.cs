ee4c1057984e1675fa34621fc1420f59908294f1
liguobao <liguobao@nana-tech.com>
2018-09-13 22:51:39 +0800

    add Controller

---
+++ House-Map.API/Controllers/NoticesController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Talk.OAuthClient;
+using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using HouseMapAPI.Dapper;
+
+namespace HouseCrawler.Web.API.Controllers
+{
+
+    [Route("v1/[controller]/")]
+    public class NoticesController : ControllerBase
+    {
+        private NoticeDapper _dapper;
+
+        public NoticesController(NoticeDapper dapper)
+        {
+            _dapper = dapper;
+        }
+
+        [EnableCors("APICors")]
+        [HttpGet("", Name = "Notice")]
+        public ActionResult Info()
+        {
+            return Ok(new { success = true, data = _dapper.FindAllNotice() });
+        }
+
+
+        [EnableCors("APICors")]
+        [HttpGet("last", Name = "FindLastNotice")]
+        public ActionResult FindLastNotice()
+        {
+            return Ok(new { success = true, data = _dapper.FindLastNotice() });
+        }
+    }
+}
