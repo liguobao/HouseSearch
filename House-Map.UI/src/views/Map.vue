a2e8ba0b143b55cfad29453e3fbc3b3accfb9753
hejianxing <hejianxing@lianj.com>
2018-09-21 14:29:09 +0800

    feat:地图部分

--- House-Map.UI/src/views/Map.vue
+++ House-Map.UI/src/views/Map.vue
@@ -1,101 +1,265 @@
 <template>
-  <div class="map" v-loading="loading">
-    <el-amap vid="amap" :zoom="zoom">
-      <el-amap-marker vid="marker"
-                      v-for="item in markers"
-                      :label="item.label"
-                      :title="item.title"
-                      :visible="item.visible"
-                      :position="item.position"
-      >
-      </el-amap-marker>
-    </el-amap>
+  <div id="app">
+
+    <div id="container"></div>
+    <div id="panel"></div>
   </div>
 </template>
-<style lang="scss" scoped>
-  .map {
+
+<style lang="scss">
+  #app {
     height: 100vh;
+    width: 100%;
+  }
+
+  #container {
+    height: 100%;
+  }
+
+  .marker-link {
+    display: block;
+    font-size: 12px;
+    color: #0e90d2;
+    text-decoration: none;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  .amap-marker .marker-route {
+    position: absolute;
+    width: 40px;
+    height: 44px;
+    color: #e90000;
+    background: url(https://webapi.amap.com/theme/v1.3/images/newpc/poi-1.png) no-repeat;
+    cursor: pointer;
+  }
+
+  .amap-marker .marker-marker-bus-from {
+    background-position: -334px -180px;
+  }
+  #panel {
+    position: absolute;
+    background-color: white;
+    max-height: 80%;
+    overflow-y: auto;
+    top: 10px;
+    right: 10px;
+    width: 250px;
+    border: solid 1px silver;
   }
 </style>
 <script>
-  import Vue from 'vue';
-  import amap from 'vue-amap';
-
-  Vue.use(amap);
-
-  amap.initAMapApiLoader({
-    // 高德key
-    key: '8a971a2f88a0ec7458d43b8bc03b6462',
-    // 插件集合 （插件按需引入）
-    plugin: ["AMap.ArrivalRange", "AMap.Scale", "AMap.Geocoder", "AMap.Transfer", "AMap.Autocomplete", "AMap.CitySearch", "AMap.Walking"],
-  });
+  import Vue from 'vue'
 
   export default {
-    name: 'Map',
     data() {
       return {
-        zoom: 13,
-        label: {
-          content: '高教花园'
-        },
+        zoom: 12,
+        info: undefined,
+        markers: [],
         loading: true,
-        geocoder: null,
-        markers: []
+        cityName: '广州',
+        myPosition: undefined,
+        transferFn: undefined
       }
     },
-    computed: {},
     methods: {
-      async init(cityName, query) {
-        let geocoder = await new AMap.Geocoder({
-          city: cityName,
-          radius: 1000
-        });
-        this.geocoder = geocoder;
-        this.getHouseList(query);
-      },
-      async getHouseList(options) {
-        let params = Object.assign({}, options, {
-          cityName: options.cityname
-        });
-        delete params.cityname;
-        let data = await this.$ajax.post(`/houses`, params);
-        let list = data.data;
-
-        let activeCode = undefined;
-        list.forEach(item => {
-          // console.log(item.houseTitle)
-          // console.log(item.houseOnlineURL)
-          // console.log(item.disPlayPrice)
-          this.geocoder.getLocation('' + item.houseTitle, (status, result) => {
-            if (status === 'complete' && result.info === 'OK') {
-              activeCode = result.geocodes[0];
-              this.markers.push(result.geocodes[0])
-              this.markers.push({
-                visible: true,
-                position: [activeCode.location.getLng(), activeCode.location.getLat()],
-                label: {
-                  content: ``
-                }
-              })
-            }
-          })
-        })
-
-        // list.forEach(item => {
-        //
-        // })
-      }
-    },
-    created() {
-
-    },
-    async mounted() {
-      this.loading = true;
-      window.onload = () => {
-        const query = this.$route.query;
-        this.init(query.cityname, query);
-        this.loading = false;
-      }
+      async getList() {
+    return await this.$ajax.post('/houses', {
+      cityName: this.cityName
+    });
+  },
+  transfer(position,map) {
+    if(this.transferFn) {
+      this.transferFn.clear();
     }
+    this.transferFn = new AMap.Transfer({
+      city: this.cityName, // 必须值，搭乘公交所在城市
+      map: map, // 可选值，搜索结果的标注、线路等均会自动添加到此地图上
+      panel: "panel", // 可选值，显示搜索列表的容器,
+      extensions: "all", // 可选值，详细信息
+      poliy: AMap.TransferPolicy.LEAST_DISTANCE // 驾驶策略：最省时模式
+    });
+
+
+    this.transferFn.search([this.myPosition.lng, this.myPosition.lat], [position.lng, position.lat],function (status,result) {
+
+    });
+
+  },
+  async init() {
+    let map = new AMap.Map('container', {
+      zoom: this.zoom,
+      resizeEnable: true,
+    });
+    map.clearMap();
+    let self = this;
+
+    AMap.plugin(['AMap.ToolBar', 'AMap.Driving', 'AMap.LineSearch', `AMap.StationSearch`, 'AMap.Transfer', 'AMap.Walking', 'AMap.Riding', 'AMap.Geolocation'], function () {//异步同时加载多个插件
+      var toolbar = new AMap.ToolBar();
+      map.addControl(toolbar);
+
+      var options = {
+        'showMarker': false,//是否显示定位按钮
+      };
+
+      var geolocation = new AMap.Geolocation(options);
+      map.addControl(geolocation);
+      geolocation.getCurrentPosition((status, result) => {
+        if (status === 'complete') {
+        self.myPosition = result.position;
+        var marker = new AMap.Marker({ //添加自定义点标记
+          map: map,
+          zIndex: 100000,
+          position: [result.position.lng, result.position.lat], //基点位置
+          offset: new AMap.Pixel(-17, -42), //相对于基点的偏移位置
+          draggable: true,  //是否可拖动
+          content: '<div class="marker-route marker-marker-bus-from"></div>'   //自定义点标记覆盖物内容
+        });
+        marker.on('dragend', function (ev) {
+          self.myPosition = ev.lnglat;
+        })
+      } else {
+
+      }
+    })
+    });
+
+
+    let code = new AMap.Geocoder({
+      city: this.cityName,
+      radius: 1000
+    });
+
+    let info = await this.getList();
+    let data = info.data;
+    data.length = 20;
+
+
+
+    let infoWindow = new AMap.InfoWindow({
+      offset: new AMap.Pixel(0, -30),
+      content: ``  //传入 dom 对象，或者 html 字符串
+    });
+    let list = await Promise.all(data.map(function (item) {
+      return new Promise((resolve, reject) => {
+        code.getLocation(item.houseLocation, (status, result) => {
+        if (status === "complete" && result.info === 'OK') {
+
+        let icon = 'https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png';
+        if (item.locationMarkBG) {
+          icon = require('./image/' + (item.locationMarkBG));
+        }
+
+        var marker = new AMap.Marker({
+          map: map,
+          title: item.houseLocation,
+          icon,
+          position: [result.geocodes[0].location.lng, result.geocodes[0].location.lat]
+        });
+
+        map.add(marker);
+
+        let displayMoney = item.DisPlayPrice ? "  租金：" + item.DisPlayPrice : "";
+        let sourceContent = item.displaySource ? " 来源：" + item.displaySource : "";
+        let title = item.houseTitle;
+        if (title) {
+          title = item.houseLocation;
+        }
+
+        marker.on('click', function (e) {
+
+          const makerInfoComponent = Vue.extend({
+            render(h) {
+              return h('div', {
+                class: ['marker-info']
+              }, [
+                h('a', {
+                  attrs: {
+                    target: '_blank',
+                    href: item.houseOnlineURL
+                  },
+                  class: ['marker-link'],
+                  domProps: {
+                    innerHTML: `房源: ${title}`
+                  }
+                }),
+                h('a', {
+                  attrs: {
+                    target: '_blank',
+                    href: item.houseOnlineURL
+                  },
+                  class: ['marker-link'],
+                  domProps: {
+                    innerHTML: `${displayMoney}`
+                  }
+                }),
+                h('a', {
+                  attrs: {
+                    target: '_blank',
+                    href: item.houseOnlineURL
+                  },
+                  class: ['marker-link'],
+                  domProps: {
+                    innerHTML: `${sourceContent}`
+                  }
+                }),
+                h('span', {
+                  domProps: {
+                    innerHTML: `开始导航`
+                  },
+                  class: ['marker-info', 'marker-link'],
+                  on: {
+                    click: function (e) {
+                      self.transfer(result.geocodes[0].location,map)
+                    }
+                  }
+                })
+              ])
+            }
+          });
+
+          const component = new makerInfoComponent().$mount();
+
+          infoWindow.setContent(component.$el);
+          infoWindow.open(map, e.target.getPosition());
+        });
+
+        resolve(marker)
+      } else {
+        resolve(item);
+        //reject(new Error('找不到坐标'))
+      }
+    })
+    })
+    }));
+
+
+  },
+  go() {
+    console.log(4)
+  }
+  },
+  async created() {
+
+  },
+  mounted() {
+    window.onload = async () => {
+      this.loading = false;
+      this.init();
+    };
+
+    this.loading = true;
+    let key = `8a971a2f88a0ec7458d43b8bc03b6462`;
+    let plugin = `AMap.ArrivalRange,AMap.Scale,AMap.Geocoder,AMap.Transfer,AMap.Autocomplete,AMap.CitySearch,AMap.Walking`.split();
+    plugin.push(`AMap.ToolBar`)
+    let url = `https://webapi.amap.com/maps?v=1.4.8&key=${key}&plugin=${plugin.join()}`;
+    let jsapi = document.createElement('script');
+    jsapi.charset = 'utf-8';
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
   }
 </script>
