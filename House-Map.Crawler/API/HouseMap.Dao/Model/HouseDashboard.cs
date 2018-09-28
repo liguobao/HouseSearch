f118bcd5d345bf945fa3ff3a22fe1f9925af62df
liguobao <liguobao@nana-tech.com>
2018-09-29 02:54:44 +0800

    fix dashboard bug

--- House-Map.Crawler/API/HouseMap.Dao/Model/HouseDashboard.cs
+++ House-Map.Crawler/API/HouseMap.Dao/Model/HouseDashboard.cs
@@ -3,17 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using HouseMap.Dao.DBEntity;
+using Newtonsoft.Json;
 
 namespace HouseMap.Models
 {
     public class HouseDashboard
     {
+        [JsonProperty(PropertyName = "cityName")]
         public string CityName { get; set; }
 
+        [JsonProperty(PropertyName = "houseSum")]
         public int HouseSum { get; set; }
 
+        [JsonProperty(PropertyName = "source")]
         public string Source { get; set; }
 
+
+        [JsonProperty(PropertyName = "displaySource")]
         public string DisplaySource
         {
             get
