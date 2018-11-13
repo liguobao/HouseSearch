5af6187801fa23ce5a37547042a965746edc0759
liguobao <liguobao@nana-tech.com>
2018-11-13 08:37:03 +0800

    fix Fangduoduo

--- House-Map.Crawler/src/NewCrawler/Fangduoduo.cs
+++ House-Map.Crawler/src/NewCrawler/Fangduoduo.cs
@@ -42,21 +42,25 @@ namespace HouseMap.Crawler
 
         private static string GetHouseListResult(string cityId, int page, string pinyin, string condition)
         {
-            var client = new RestClient("https://m.fangdd.com/api/zufang/fetchRentList?cityId=121");
+            var client = new RestClient("https://m.fangdd.com/api/data/zufangFetchRentList");
             var request = new RestRequest(Method.POST);
-            request.AddHeader("referer", $"https://m.fangdd.com/{pinyin}/zufang-list/");
+            request.AddHeader("referer", $"https://m.fangdd.com/{pinyin}/zufang-list-v3/");
             request.AddHeader("cache-control", "no-cache");
             request.AddHeader("accept", "application/json, text/plain, */*");
             request.AddHeader("content-type", "application/json;charset=UTF-8");
-            request.AddHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
+            request.AddHeader("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
+            request.AddHeader("session-id", "85741e90e02d11e89df59da5272c0b71");
+            request.AddHeader("fdd_trace_id", "4da9f390e6db11e8aaccaf47ed81949d");
             request.AddHeader("pragma", "no-cache");
             request.AddHeader("connection", "keep-alive");
+            request.AddHeader("x-requested-with", "XMLHttpRequest");
             request.AddHeader("client", "m");
             request.AddHeader("fa", "FA1.0.1541334670781.4122388242");
             request.AddHeader("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,da;q=0.7");
             request.AddHeader("accept-encoding", "gzip, deflate, br");
             request.AddHeader("origin", "https://m.fangdd.com");
             request.AddHeader("city-id", cityId);
+            request.AddHeader("cookie", "deviceId=85741e90e02d11e89df59da5272c0b71; _fa=FA1.0.1541334670781.4122388242; _ga=GA1.2.2103099985.1541334671; cacheCity=guangzhou; TP_AGENT_ID=5be98035ec250fb6a2015bc6; cityName=\"%E6%B7%B1%E5%9C%B3\"; cityPinYin=shenzhen; cityPy=sz; city_id=1337; client=M; _F=m; _T=%E7%A7%9F%E6%88%BF%E6%88%BF%E6%BA%90%E8%AF%A6%E6%83%85; _gid=GA1.2.1662368975.1542029367; Hm_lvt_3c55bb2c7ecabdb4d55e6539eaa2f3ab=1541334671,1542029370; _ha=1542068945647.5216018004; Hm_lpvt_3c55bb2c7ecabdb4d55e6539eaa2f3ab=1542069000");
             request.AddHeader("user-id", "0");
             string query_condition = condition.Replace("p1", "p" + (page + 1));
             request.AddParameter("application/json;charset=UTF-8", "{\"cityId\":" + cityId + ",\"condition\":\"" + query_condition + "\"}", ParameterType.RequestBody);
@@ -130,7 +134,7 @@ namespace HouseMap.Crawler
 
         private static int GetRentType(JToken room)
         {
-            var rentType = room["rentType"].ToObject<int>();
+            var rentType = room["rentType"]?.ToObject<int>();
             if (rentType == 2)
             {
                 return (int)RentTypeEnum.Shared;
